c6d3d5c3d034f51ab45a8c2c4b91be9b2fe11d01
oscarArismendi <oscarfernandoarismendigalvis@gmail.com>
2024-09-19 15:39:07 -0500

    feat: made a dynamic button with usage documentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
-import './App.css'
-
+import './App.css';
+import ButtonDemo from './examples/ButtonDemo';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl font-bold underline">
+    <><h1 className="text-3xl font-bold underline">
       It's working!
     </h1>
+    <ButtonDemo></ButtonDemo>
+    </>
   )
 }
 
